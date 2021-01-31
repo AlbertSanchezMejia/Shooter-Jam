3d1b9ddbf5e44dc06701c440e8770a297d114ad2
elgranguerrero <tony14_ucsm@hotmail.com>
2021-01-31 08:39:59 -0500

    Avance de la pared y piso

---
+++ Assets/Scripts/Torre/DetectarEnemigos.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectarEnemigos : MonoBehaviour
+{
+    public GameObject genBalas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "enemigo")
+        {
+            genBalas.GetComponent<GenPlasma>().disparar();
+            Debug.Log("Funciono el disparo");
+        }
+    }
+}
